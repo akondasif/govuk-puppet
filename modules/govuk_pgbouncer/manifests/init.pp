aa953c6d8539b8af2d2bc6955159da44414c7c3f
Paul Bowsher <paul.bowsher@digital.cabinet-office.gov.uk>
2018-07-23 16:34:27 +0100

    WIP
    Next:
    - Create new pg_hba for pgbouncer which excludes ident (ident incompatible, unhelpful error message)
    - Create userlist based on list of databases. vagrant needs an empty password, with "trust" allowed.

--- modules/govuk_pgbouncer/manifests/init.pp
+++ modules/govuk_pgbouncer/manifests/init.pp
@@ -8,13 +8,26 @@
 #   A list of databases for pgbouncer to connect to
 #
 class govuk_pgbouncer(
-  $databases = [],
+  # $databases = [],
 ) {
+
+  $databases = [
+    {
+      source_db => 'publishing_api_development',
+      dest_db   => 'publishing_api_development',
+      host      => '127.0.0.1',
+      auth_user => 'vagrant',
+    }
+  ]
 
   class { '::pgbouncer':
     user      => 'postgres',
     group     => 'postgres',
     databases => $databases,
-    userlist  => [{user => 'foo', password => 'bar'}]
+    userlist  => false,
+    config_params => {
+      auth_type => 'hba',
+      auth_hba_file => "/etc/postgresql/${::postgresql::globals::version}/main/pg_hba.conf"
+    }
   }
 }
